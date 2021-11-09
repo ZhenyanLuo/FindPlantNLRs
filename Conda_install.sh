4209e43f7909f548992af0d357d22c99349dd6fb
Zhenyan Luo <53864342+ZhenyanLuo@users.noreply.github.com>
2021-11-09 17:08:42 +1100

    Update Conda_install.sh

--- Conda_install.sh
+++ Conda_install.sh
@@ -1,12 +1,14 @@
-conda install -y seqkit
-conda install -y bedtools
-conda install -y samtools
-conda install -c bioconda -y blast
-conda install -c bioconda -y pfam_scan
-conda install -c bioconda -y augustus
+conda create -n NLR -y
+conda install -c bioconda snakemake=6.10.0 -y
+conda install python=3.6.5 -y
+conda install -c bioconda bedtools=2.3.0 -y
+conda install -c bioconda samtools=1.9 -y
+conda install -c bioconda clustalo=1.2.4 -y
+conda install -c bioconda hmmer=3.3.2 -y
+conda install -c bioconda blast=2.7.1 -y
+conda install -c bioconda seqkit=2.0.0 -y
 #download interoproscan#
 wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.48-83.0/interproscan-5.48-83.0-64-bit.tar.gz
 gunzip interproscan-5.48-83.0-64-bit.tar.gz
 git clone https://github.com/Gaius-Augustus/BRAKER.git
 git clone https://github.com/steuernb/NLR-Annotator.git
-git clone https://github.com/peritob/Myrtaceae_NLR_workflow.git
