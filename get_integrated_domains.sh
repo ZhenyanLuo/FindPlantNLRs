f3753896b9e15e082323e5ae48307f1db680622a
Yan <53864342+ZhenyanLuo@users.noreply.github.com>
2022-02-10 16:00:27 +1100

    Create get_integrated_domains.sh

---
+++ get_integrated_domains.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Copy from https://github.com/peritob/Myrtaceae_NLR_workflow/blob/master/get_integrated_domains.sh
+
+prefix_name=$1
+NLR_type=$2
+
+#First extract all Pfam codes from the .tsv - thereby excluding all coils and other database outputs.
+grep 'Pfam' ${prefix_name}_augustus_aa.fasta.tsv > ${prefix_name}_pfam.tsv
+#Then extract all the genes based on the lists from NLR_clasification. For example,
+fgrep -w -f ${prefix_name}_${NLR_type}.list ${prefix_name}_pfam.tsv > ${prefix_name}_${NLR_type}_Pfams.tsv
+#Then extract all domains that are not present in NLR classifications using the parameter -v with fgrep. Make a list of NLR Pfams and call the txt file "NLR_Pfams.list" (PF00931, PF18052, PF08263, PF12799, PF13306, PF13855, PF13516, PF00560, PF07725, PF05659, PF01582, PF17862) associated with NLR genes.
+fgrep -wv -f NLR_Pfams.list ${prefix_name}_${NLR_type}_Pfams.tsv > ${prefix_name}_${NLR_type}_ID_Pfams.tsv
+#Obtain meaningful column data 
+gawk 'BEGIN {FS="\t"; OFS="\t"}; {print $1, $5, $6, $7, $8}' ${prefix_name}_${NLR_type}_ID_Pfams.tsv > ${prefix_name}_${NLR_type}_ID_Pfams.data
